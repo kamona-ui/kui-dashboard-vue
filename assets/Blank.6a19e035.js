a621d3b925e41ee9ccfa3f235142ef51c0195b97
Ahmed Kamel <ak3849909@gmail.com>
2022-07-07 06:51:49 +0200

    Updates

--- assets/Blank.6a19e035.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./PageWrapper.2de636a8.js";import{e as r,f as a}from"./vendor.87ee42be.js";const _={setup(t){return(o,s)=>(r(),a(e,{title:"Blank"}))}};export{_ as default};
---
+++ assets/Blank.8b35262b.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./PageWrapper.e136cce1.js";import{o as t,c as e}from"./index.a2ff0962.js";const s={__name:"Blank",setup(o){return(r,_)=>(t(),e(a,{title:"Blank"}))}};export{s as default};
