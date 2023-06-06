19b992c177607b7a81700e1a2513087858567e69
Ahmed Kamel <ak384909@gmail.com>
2023-06-06 17:48:28 +0300

    Updates

--- assets/Blank.8b35262b.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./PageWrapper.e136cce1.js";import{o as t,c as e}from"./index.a2ff0962.js";const s={__name:"Blank",setup(o){return(r,_)=>(t(),e(a,{title:"Blank"}))}};export{s as default};
