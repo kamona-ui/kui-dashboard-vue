ced55719b9bb92d499a8ac91cb560cdbcce2da75
Kamona-WD <ak3849909@gmail.com>
2025-01-27 17:40:02 +0200

    Updates

---
+++ assets/Blank-CDDXacl9.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./PageWrapper-N8IcIb89.js";import{o as t,h as e}from"./index-Bl2R4E9n.js";const s={__name:"Blank",setup(o){return(r,_)=>(t(),e(a,{title:"Blank"}))}};export{s as default};
--- assets/Blank.409ec944.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./PageWrapper.d5e4ef31.js";import{o as t,c as e}from"./index.b21598e3.js";const s={__name:"Blank",setup(o){return(r,_)=>(t(),e(a,{title:"Blank"}))}};export{s as default};
